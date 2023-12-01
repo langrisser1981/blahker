2a5a91368959bb1580b198a46d8a7975194bbcbf
lenny_cheng <langrisser1981@gmail.com>
2023-12-01 17:22:57 +0800

    add package

---
+++ MyLibrary/Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import ComposableArchitecture
+import SwiftUI
+
+@main
+struct Blahker: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("hello world")
+        }
+    }
+}
